1a37ae1cbf8e121f14273f64491ea08b15b757df
Spoonerisms <kirstenaobrien@gmail.com>
2022-09-10 16:50:37 -0700

    Update fallbot.py
    Fallbot 2022 version

--- fallbot.py
+++ fallbot.py
@@ -1,15 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import random
 import tweepy
 
 from pytz import timezone, utc
+
 from time import sleep
+
 from datetime import datetime, timedelta, date
+
+CONSUMER_KEY = 'eMVZBpgEJ75JC1xyNyzVQ6sO4'
+CONSUMER_SECRET = 'kuluz9Wrc8U6hfOOoSafGtsWf6tUAojL1sQjyH5BNPkdIjqlen'
+ACCESS_TOKEN = '897666750640029697-UjxY6VOGjLDTkjRCT9So87bUrzzilR6'
+ACCESS_TOKEN_SECRET = 'Bss1XKBoRq8LWW6n9zDLYKSQirBeBel0fSZoKNP7pDXpR'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
 
 """
 setting this as the constant for TAPI
@@ -18,22 +23,29 @@ TAPI = tweepy.API(auth)
 
 ###
 PHRASE_BOOK = [
-    "My favorite season starts in %s",
-    "Only %s until fall",
-    "I only have to wait %s until fall",
-    "Hot toddies for everyone when fall starts in %s",
-    "Summer sucks",
-    "Fall starts in %s",
+    'Fall starts in {}',
+    'Days till fall: {}',
+    'My favorite season starts in {}',
+    'No pumpkin spice lattes for the next {}',
+    'I cant wait to drink a hot toddy in {}',
+    'FML for the next {}',
+    'Leaves start changing color in {}',
+    'We only have to wait {} until fall',
+    'The best season starts in {}',
+    "Hey guys Fall here, I'm coming for you in {}",
 ]
-FALL_START = datetime(2019, 9, 23)
+# FALL_START = datetime(2021, 9, 22)
+SUMMER_START_DAY = 170
+FALL_START_DAY = 265
 
-LAST_RUN_LOG_FILE = 'last_run.log'
+LAST_RUN_LOG_FILE = '/Users/Kirsten/Desktop/KIRSTEN/last_run.log'
 
 ### Check if ran today - if false then maybe we should run
 def ran_today():
     try:
         with open(LAST_RUN_LOG_FILE, 'r') as f:
-            d = datetime.strptime(f.read(), "%Y-%m-%d").date()
+            datestring = f.readlines()[0].rstrip()
+            d = datetime.strptime(datestring, "%Y-%m-%d").date()
             if datetime.now().date() == d:
                 return True
             return False
@@ -48,8 +60,9 @@ def log_today():
 
 def get_time_till_fall():
     now = datetime.now()
-    d = FALL_START - now
+    fall_start = datetime(now.year, 1, 1) + timedelta(FALL_START_DAY - 1)
 
+    d = fall_start - now
     # string formatting to make it look good
     d = str(d).split('.')[0] # remove miliseconds
     s = d.split(':')
@@ -65,28 +78,36 @@ def randomize_day():
         We could normalize this but I like what we've got so far.
         Ex: Chance to run at midnight, very low, chance to run at noon 50%, chance to run at 11:59pm 100%.
     """
-
     seconds_in_day = 86400
     n = datetime.now()
     seconds_today = n.hour * 3600 + n.minute * 60 + n.second
+    #if seconds_today < 32400
+        #return False
     r = int(random.random() * 86400)
     if r < seconds_today:
         # sleep for a random 6 hours to hide the fact it runs on 6 hour interval
-        #sleep(random.random() * 300)
+        sleep(random.random() * 300)
         return True
     return False
 
 def post_to_twitter():
     p = random.choice(PHRASE_BOOK).format(get_time_till_fall())
-
     print (p)
     TAPI.update_status(status=p)
 
 def run():
-    if not ran_today() and randomize_day():
-        post_to_twitter()
-        log_today()
+    doy = datetime.today().timetuple().tm_yday
+    if doy == FALL_START_DAY:
+        if not ran_today() and randomize_day():
+            msg = "IT'S FALL"
+            TAPI.update_status(status=msg)
+            log_today()
+    if doy > SUMMER_START_DAY and doy < FALL_START_DAY:
+        if not ran_today() and randomize_day(): #makes the time bot tweets seem "random"
+            post_to_twitter()
+            log_today()
 
+            
 if __name__ == "__main__":
     """
     We assume crontab is run every 5 minutes
